5a24db897d23637482ddea475a1e95016a65abf5
lukesilvia <lukesilvia@gmail.com>
2018-11-26 23:21:33 +0900

    log messages when starting server, receiving http request.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const http = require ( "http" );
 
 http.createServer( ( request, response ) => {
+  console.log( "Request received." );
   response.writeHead( 200, { "Content-Type": "text/plain" } );
   response.write( "Hello, World" );
   response.end();
 } ).listen( 8888 );
+
+console.log( "Server has started." );
