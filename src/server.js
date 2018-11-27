d5d5a71470aecc5933760f57e378a944444614c8
lukesilvia <lukesilvia@gmail.com>
2018-11-27 19:58:36 +0900

    define callback function as named function

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const http = require ( "http" );
 
-http.createServer( ( request, response ) => {
+const onRequest = ( request, response ) => {
 	console.log( "Request received." );
 	response.writeHead( 200, { "Content-Type": "text/plain" } );
 	response.write( "Hello, World" );
 	response.end();
-} ).listen( 8888 );
+};
+
+http.createServer( onRequest ).listen( 8888 );
 
 console.log( "Server has started." );
 
