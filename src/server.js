73fed430266d9535ba3bb945a015d26f7a61af52
lukesilvia <lukesilvia@gmail.com>
2018-11-27 14:52:33 +0900

    add .editorconfig, change indent style to tab

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const http = require ( "http" );
 
 http.createServer( ( request, response ) => {
-  console.log( "Request received." );
-  response.writeHead( 200, { "Content-Type": "text/plain" } );
-  response.write( "Hello, World" );
-  response.end();
+	console.log( "Request received." );
+	response.writeHead( 200, { "Content-Type": "text/plain" } );
+	response.write( "Hello, World" );
+	response.end();
 } ).listen( 8888 );
 
 console.log( "Server has started." );
+
