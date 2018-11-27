0e1c692eba8dc35adb3adf167ebf471f44a104ef
lukesilvia <lukesilvia@gmail.com>
2018-11-27 20:23:38 +0900

    add router for routing request to hander:

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const http = require ( "http" );
+const url  = require ( "url" );
 
-const start = () => {
+const start = ( route ) => {
 	const onRequest = ( request, response ) => {
-		console.log( "Request received." );
+		var pathname = url.parse( request.url ).pathname;
+		console.log( `Request for ${pathname} received.` );
+
+		route( pathname );
+
 		response.writeHead( 200, { "Content-Type": "text/plain" } );
 		response.write( "Hello, World" );
 		response.end();
@@ -14,5 +19,3 @@ const start = () => {
 };
 
 exports.start = start;
-
-
