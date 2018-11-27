02fe3f49e1853bb736b46b926fc2ab6a57bdfc93
lukesilvia <lukesilvia@gmail.com>
2018-11-27 20:04:18 +0900

    modulize server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 const http = require ( "http" );
 
-const onRequest = ( request, response ) => {
-	console.log( "Request received." );
-	response.writeHead( 200, { "Content-Type": "text/plain" } );
-	response.write( "Hello, World" );
-	response.end();
+const start = () => {
+	let onRequest = ( request, response ) => {
+		console.log( "Request received." );
+		response.writeHead( 200, { "Content-Type": "text/plain" } );
+		response.write( "Hello, World" );
+		response.end();
+	};
+
+	http.createServer( onRequest ).listen( 8888 );
+
+	console.log( "Server has started." );
 };
 
-http.createServer( onRequest ).listen( 8888 );
+exports.start = start;
 
-console.log( "Server has started." );
 
