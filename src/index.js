03525674a97dccb5314b271f4a0c31a31f421382
lukesilvia <lukesilvia@gmail.com>
2018-12-02 10:35:13 +0900

    route detects request handlers by pathname

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const server = require ( "./server" );
 const router = require ( "./router" );
+const handlers = require ( "./handlers" );
 
-server.start( router.route );
+const handle = {};
+handle[ "/" ]       = handlers.start;
+handle[ "/start" ]  = handlers.start;
+handle[ "/upload" ] = handlers.upload;
+
+server.start( router.route, handle );
--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const http = require ( "http" );
 const url  = require ( "url" );
 
-const start = ( route ) => {
+const start = ( route, handle ) => {
 	const onRequest = ( request, response ) => {
 		var pathname = url.parse( request.url ).pathname;
 		console.log( `Request for ${pathname} received.` );
 
-		route( pathname );
+		route( handle, pathname );
 
 		response.writeHead( 200, { "Content-Type": "text/plain" } );
 		response.write( "Hello, World" );
