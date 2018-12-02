7f6e69657dee9826952919286f803f73f70979d9
lukesilvia <lukesilvia@gmail.com>
2018-12-02 10:49:39 +0900

    server reads response text from handler

--- src/handlers.js
+++ src/handlers.js
@@ -1,9 +1,11 @@
 const start = () => {
 	console.log( "Request handler 'start' was called." );
+	return "Start";
 };
 
 const upload = () => {
 	console.log( "Request handler 'upload' was called." );
+	return "Upload";
 };
 
 exports.start = start;
